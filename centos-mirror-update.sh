0da1fce066a370365be68fe62d4ecef890363cd8
Jeff Pettorino <0xSheepdog@gmail.com>
2019-05-21 15:04:58 -0600

    added script outlines

--- centos-mirror-update.sh
+++ centos-mirror-update.sh
@@ -1,3 +1,4 @@
+#!/bin/#!/bin/sh
 reposync -gdlmq -p /data/repos/centos7 --repoid=updates --download-metadata
 createrepo -p -o /data/repos/centos7/updates --baseurl=http://mirror.spde.local/centos7/updates --update /data/repos/centos7/updates
 reposync -gdlmq -p /data/repos/centos7 --repoid=extras --download-metadata
