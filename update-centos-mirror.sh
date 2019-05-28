d733b5f4064cc55998f455b1e9e79b20b528aeb3
0xSheepdog <0xSheepdog@gmail.com>
2019-05-28 14:15:11 -0600

    New and renamed files.

--- update-centos-mirror.sh
+++ update-centos-mirror.sh
@@ -1,7 +1,9 @@
 #!/bin/#!/bin/sh
-reposync -gdlmq -p /data/repos/centos7 --repoid=updates --download-metadata
+reposync -gdlmq --download-metadata -p /data/repos/centos7
+#reposync -gdlmq --download-metadata -p /data/repos/centos7 --repoid=updates
+#reposync -gdlmq --download-metadata -p /data/repos/centos7 --repoid=extras
+#reposync -gdlmq --download-metadata -p /data/repos/centos7 --repoid=base
+
 createrepo -p -o /data/repos/centos7/updates --baseurl=http://mirror.spde.local/centos7/updates --update /data/repos/centos7/updates
-reposync -gdlmq -p /data/repos/centos7 --repoid=extras --download-metadata
 createrepo -p -o /data/repos/centos7/extras --baseurl=http://mirror.spde.local/centos7/extras --update /data/repos/centos7/extras
-reposync -gdlmq -p /data/repos/centos7 --repoid=base --download-metadata
 createrepo -p -g /data/repos/centos7/base/Packages/comps.xml -o /data/repos/centos7/base --baseurl=http://mirror.spde.local/centos7/base --update /data/repos/centos7/base
