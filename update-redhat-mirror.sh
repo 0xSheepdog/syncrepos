9d62e2d139497496a500d329cf95143756babd4e
0xSheepdog <0xSheepdog@gmail.com>
2019-05-29 13:03:12 -0600

    remove yml

--- update-redhat-mirror.sh
+++ update-redhat-mirror.sh
@@ -5,8 +5,6 @@ reposync -gdlmnq --download-metadata -p /net/host11.spde.local/data/repos/redhat
 #reposync -gdlmnq --download-metadata -p /net/host11.spde.local/data/repos/redhat --repoid=rhel-7-server-extras-rpms
 #reposync -gdlmnq --download-metadata -p /net/host11.spde.local/data/repos/redhat --repoid=rhel-7-server-ansible-2-rpms
 
-
-
 /bin/createrepo -p -g /data/repos/redhat/rhel-7-server-rpms/comps.xml -o /data/repos/redhat/rhel-7-server-rpms --baseurl=http://mirror.spde.local/redhat/rhel-7-server-rpms /data/repos/redhat/rhel-7-server-rpms
 /bin/createrepo -p -g /data/repos/redhat/rhel-7-server-extras-rpms/comps.xml -o /data/repos/redhat/rhel-7-server-extras-rpms --baseurl=http://mirror.spde.local/redhat/rhel-7-server-extras-rpms /data/repos/redhat/rhel-7-server-extras-rpms
 /bin/createrepo -p -g /data/repos/redhat/rhel-7-server-optional-rpms/comps.xml -o /data/repos/redhat/rhel-7-server-optional-rpms --baseurl=http://mirror.spde.local/redhat/rhel-7-server-optional-rpms /data/repos/redhat/rhel-7-server-optional-rpms
