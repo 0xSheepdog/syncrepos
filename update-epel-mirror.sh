e98079591130f5d1315875db8996cfc554563a03
Jeff Pettorino <0xSheepdog@gmail.com>
2019-09-10 12:07:03 -0600

    new repo script

--- update-epel-mirror.sh
+++ update-epel-mirror.sh
@@ -3,6 +3,11 @@
 
 yum-config-manager --disable \*
 yum-config-manager --enable epel
-reposync -gdlm --download-metadata -p /data/repos/epel --repoid=epel
+reposync -gdlm --download-metadata --norepopath -p /data/repos/epel --repoid=epel
 yum-config-manager --disable epel
 yum-config-manager --enable base extras updates
+
+## createrepo -p -g /data/repos/epel/comps.xml -o /data/repos/epel --baseurl=http://mirror.spde.local/epel
+
+
+createrepo -p --update -g /data/repos/epel/comps.xml -o /data/repos/epel /data/repos/epel
